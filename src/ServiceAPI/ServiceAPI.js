58672ae7b2a140bae5c5f2b6d0510f1381475908
YuriyEG <you@example.com>
2023-09-28 05:29:53 +0300

    pagination is done

---
+++ src/ServiceAPI/ServiceAPI.js
@@ -0,0 +1,58 @@
+
+class ServiceApi {
+
+    constructor() {
+      this.baseUrl = 'https://blog.kata.academy/api/';
+      this.forUsers = 'users/'
+      this.forArticles = 'articles/';
+    }
+    defaultResponce = () => {
+      console.log('Передайте колбэк для респонса!');
+    }
+    defaultError = () => {
+      console.log('Передайте колбэк для ошибки');
+    }
+    createUser = (username, email, password, onResponse = this.defaultResponce, onError = this.defaultError ) => {
+        let obj = {
+          user: {
+            username: username,
+            email: email,
+            password: password
+          }
+        }
+  
+        fetch(this.baseUrl + this.forUsers,{
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(obj)
+  }
+  )
+    .then( res => res.json())
+    .then( res => onResponse(res))
+    .catch( err => onError(err))
+  
+    }
+  
+    getArticles = (onResponse, onError, limit = 5, offset = 0 ) => {
+  
+      fetch(this.baseUrl + this.forArticles + `?limit=${limit}&&offset=${offset}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8"
+        }
+      })
+        .then( res => res.json())
+        .then( res => onResponse(res))
+        .then( err => onError(err))
+  
+    }
+    
+  }
+  
+  
+  
+  let key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1M…jg1fQ.JKGQtlidCcXFe06ChHGUGXQeJdG4PkTs7rmaX0qthnU';
+
+  export default ServiceApi;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,22 +2,30 @@
 
 import React, { useEffect, useState } from 'react';
 import { Pagination } from 'antd';
+import ServiceAPI from '../../ServiceAPI/ServiceAPI';
+
 
 import ArticleItem from '../ArticleItem';
 
 import styles from './list.module.css';
+const service = new ServiceAPI();
 
 const List = () => {
   const [articles, setArticles] = useState([1, 3, 4]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [offset, setOffset] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  const dataReceiver = (data) => {
+    setArticles(data.articles);
+    console.log(data);
+    setTotalPages(Math.ceil(data.articlesCount/5))
+  }
+
 
   useEffect(() => {
-    fetch('https://blog.kata.academy/api/articles')
-      .then((res) => res.json())
-      .then((res) => {
-        setArticles(res.articles);
-        console.log(res);
-      });
-  }, []);
+    service.getArticles((res) => dataReceiver(res), (err) => console.log(err), 5, (currentPage-1)*5);
+  }, [currentPage]);
   
   return (
     <div className={styles.list}>
@@ -25,7 +33,12 @@ const List = () => {
         <ArticleItem article={article} key={Math.random()*Date.now()}/>
       ))}
       <div className={styles.list__pagination}>
-        <Pagination />
+        <Pagination defaultCurrent={0}
+          pageSize={1}
+          total={totalPages}
+          showSizeChanger={false}
+          onChange={(page) => setCurrentPage(page)}
+        />
       </div>
     </div>
   );
