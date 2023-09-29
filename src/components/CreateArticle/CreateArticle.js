2b837e9477728dbbc0cb8a2d82147650b47166e2
YuriyEG <you@example.com>
2023-09-29 06:07:24 +0300

    sdfsfds

--- src/components/CreateArticle/CreateArticle.js
+++ src/components/CreateArticle/CreateArticle.js
@@ -15,7 +15,7 @@ const service = new ServiseAPI();
 
 const CreateArticle = () => {
 
-  const [tags, setTags] = useState(['one', 'two']);
+  const [tags, setTags] = useState([]);
   const [curTag, setCurTag] = useState('');
 
   const {
@@ -114,12 +114,12 @@ const CreateArticle = () => {
           <input className={styles.createArticle__tagInput} />
           <DeleteButton value="Delete" />
         </div> */}
-        {/* { 
+        { 
           tags.map( (tag) => (<div>
           <input className={styles.createArticle__tagInput} value={tag} />
           <DeleteButton value="Delete" />
         </div>))
-        } */}
+        }
         <div className={styles.createArticle__tagWrapper}>
           <input value={curTag} className={styles.createArticle__tagInput} onChange={(e) => setCurTag(e.target.value)}/>
           <button className={styles.addTagButton} onClick={(e) => addTag(e)}>Add tag</button>
@@ -140,7 +140,7 @@ const CreateArticle = () => {
           <DeleteButton value="Delete" />
           <AddTagButton />
         </div> */}
-        <button className={styles.sendButton}>Send</button>
+        <button type="submit" className={styles.sendButton}>Send</button>
         
       </form>
     </div>
