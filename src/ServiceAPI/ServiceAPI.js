cfc5d251d6b90d56ec3eb22f1e32a9f8c7fe3c2c
YuriyEG <you@example.com>
2023-09-29 03:15:05 +0300

    new Article api created

--- src/ServiceAPI/ServiceAPI.js
+++ src/ServiceAPI/ServiceAPI.js
@@ -36,11 +36,39 @@ class ServiceApi {
         .then( err => onError(err))
   
     }
+
+    createArticle ( receivedArticle, apiKey,  onResponse = () => console.log('Не передан колбэк для респонса'), onError = () => console.log('не передан колбэк для ошибок')) {
+
+        fetch(this.baseUrl + this.forArticles, {
+          method: "POST",
+          headers: {
+  
+            "Content-Type": "application/json",
+            "Authorization": `Token ${apiKey}`,
+          },
+          body: JSON.stringify({ article: receivedArticle }),
+        })
+          .then( res => res.json())
+          .then( res => onResponse(res))
+          .catch( err => onError(err))
+    }
     
   }
-  
-  
-  
   let key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1M…jg1fQ.JKGQtlidCcXFe06ChHGUGXQeJdG4PkTs7rmaX0qthnU';
+  const service = new ServiceApi();
+  service.createArticle({
+    
+      "title": "Зачем разработчикам React",
+      "description": "React — самая популярная библиотека ",
+      "body": "React — это инструмент для создания пользовательских интерфейсов. Его главная задача — обеспечение вывода на экран того, что можно видеть на веб-страницах. React значительно облегчает создание интерфейсов благодаря разбиению каждой страницы на небольшие фрагменты. Мы называем эти фрагменты компонентами",
+      "tags": [
+        "string"
+      ]
+    
+  }, key, (res) => console.log(res), (err) => console.log(err))
+  
+  
+  
+
 
   export default ServiceApi;
