ea1ef9320116304dbb1e385a7e4c688b0c2a578f
YuriyEG <you@example.com>
2023-10-03 02:54:40 +0300

    85%

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { Navigate } from "react-router-dom";
+import { Switch, Redirect } from 'react-router-dom';
 
 import Header from '../Header';
 import CreateArticle from '../CreateArticle';
@@ -14,6 +15,7 @@ import SignIn from '../SignIn';
 import EditProfile from '../EditProfile';
 import SignUp from '../SignUp';
 import Article from '../Article';
+
 
 
 import styles from './App.module.css';
@@ -32,12 +34,15 @@ const App = () => {
   }, (err) => console.log(err));
  }, [])
   
- console.log(localStorage.getItem('isAuth'));
+ 
 
   return (
     <Router>
       <div className={styles.app}>
         <Header path="/" exact auth={auth} setAuth={setAuth} />
+        <Switch>
+
+     
         <Route path="/"  component={List} exact />
         <Route path="/articles" component={List} exact />
         <Route path="/articles/:id" render={
@@ -51,15 +56,25 @@ const App = () => {
           return <SignIn auth={auth} setAuth={setAuth} />
         }}  exact/>
         <Route path="/profile" component={EditProfile} exact/>
-        <Route path="/new-article" component={CreateArticle} exact />
+        <Route path="/new-article" exact 
+          render={ () => {
+            return <CreateArticle auth={auth}/>
+          }}
+        />
         <Route path="/articles/:slug/edit" render={
+
           ({match, location, history }) => {
+            if (auth === 'false') {
+              history.push('/sign-in');
+            }
             console.log(match);
             const { slug } = match.params;
             return <EditArticle slug={slug} exact />
           }
         }/>
-        {/* <Route path="/articles/{slug}/edit" component={EditArticle} exact /> */}
+        <Route render={ () => <h1 style={{ marginTop: '50px', marginLeft: '40%'}}>Page not found!</h1>}/>
+                </Switch>
+   
 
 
       
